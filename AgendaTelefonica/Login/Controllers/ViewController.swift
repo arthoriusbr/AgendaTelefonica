be4206adbd35efede774e4a2903690b82127a946
administrador <administrador@Mac-mini-de-administrador.local>
2018-07-18 11:54:19 -0300

    projeto associado ao login com o serviço e agora ao clicar em Entrar aparece na saída do xcode (com o print mostrando email e id feito no LoginService)

--- AgendaTelefonica/Login/Controllers/ViewController.swift
+++ AgendaTelefonica/Login/Controllers/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - Vars
+    var service: LoginService!
 
     @IBOutlet weak var matriculaLogin: UITextField!
     @IBOutlet weak var senhaLogin: UITextField!
@@ -17,27 +20,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.service = LoginService()
+        
         self.matriculaLogin.placeholder = L10n.Login.matricula
         self.senhaLogin.placeholder = L10n.Login.senha
         
         self.botaoEntrarLogin.setTitle(L10n.Login.entrar, for: .normal)
-        self.botaoEntrarLogin.layer.borderWidth = 1/UIScreen.main.nativeScale
-        self.botaoEntrarLogin.layer.borderColor = UIColor.blue.cgColor
-        self.botaoEntrarLogin.contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+
+//        //para fazer apenas uma borda para o botao
+//        self.botaoEntrarLogin.layer.borderWidth = 1/UIScreen.main.nativeScale
+//        self.botaoEntrarLogin.layer.borderColor = UIColor.blue.cgColor
+//        self.botaoEntrarLogin.contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
         
         self.botaoCadastrarLogin.setTitle(L10n.Login.cadastrar, for: .normal)
-        self.botaoCadastrarLogin.layer.borderWidth = 1/UIScreen.main.nativeScale
-        self.botaoCadastrarLogin.layer.borderColor = UIColor.blue.cgColor
-        self.botaoCadastrarLogin.contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
         
+//        //para fazer apenas uma borda para o botao
+//        self.botaoCadastrarLogin.layer.borderWidth = 1/UIScreen.main.nativeScale
+//        self.botaoCadastrarLogin.layer.borderColor = UIColor.blue.cgColor
+//        self.botaoCadastrarLogin.contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
         
         self.matriculaLogin.layer.cornerRadius = self.matriculaLogin.frame.height / 2
         self.senhaLogin.layer.cornerRadius = self.senhaLogin.frame.height / 2
-        self.botaoEntrarLogin.layer.cornerRadius = self.botaoEntrarLogin.frame.height / 4
-        self.botaoCadastrarLogin.layer.cornerRadius = self.botaoCadastrarLogin.frame.height / 4
+        self.botaoEntrarLogin.layer.cornerRadius = self.botaoEntrarLogin.frame.height / 2
+        self.botaoCadastrarLogin.layer.cornerRadius = self.botaoCadastrarLogin.frame.height / 2
     }
 
     @IBAction func logar(_ sender: Any) {
+        if let email = self.matriculaLogin.text, let senha = self.senhaLogin.text {
+            self.service.postLogin(email: email, senha: senha)
+        }
         
     }
 }
--- AgendaTelefonica/Login/Service/LoginService.swift
+++ AgendaTelefonica/Login/Service/LoginService.swift
@@ -6,11 +6,25 @@
 //  Copyright © 2018 administrador. All rights reserved.
 //
 
-import Foundation
+import Alamofire
+import AlamofireObjectMapper
 
 class LoginService {
     
     func postLogin(email: String, senha: String) {
-//        LoginRequestFactory.postLogin(email: email, senha: senha).validate()
+        LoginRequestFactory.postLogin(email: email, senha: senha).validate().responseObject(keyPath: "data") { (response: DataResponse<User>) in
+            
+            switch response.result {
+            case .success:
+                
+                if let user = response.result.value {
+                    print("Email \(user.email ?? "") com id \(user.id ?? 0)")
+                                        
+                    user.setHeaderParams(header: response.response?.allHeaderFields)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
